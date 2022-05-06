21b2eb1ae1021c7ed725e47651414bebf1e448f4
om-prakash-yadav <jnvomprakash121@gmail.com>
2022-05-06 21:15:53 +0530

    data structures and algorithms practice starts

---
+++ ArrayProblem1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    int mx=INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for(int i=0; i<n; i++)
+    {
+        mx=max(mx,arr[i]);
+        cout<<mx<<" ";
+    }
+    return 0;
+}
